e4a9823fad1d5a408a70e2425e945f76c0a202aa
huseynmurtuzov <murtuzovhuseyn2@gmail.com>
2025-06-02 15:44:23 +0400

    catalog microservice added

---
+++ Services/Catalog/ShopApp.Catalog/Dtos/ProductDetailsDtos/CreateProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.Catalog.Dtos.ProductDetailsDtos
+{
+    public class CreateProductDetailDto
+    {
+        public string ProductDescription { get; set; }
+        public string ProductInformation { get; set; }
+        public string ProductId { get; set; }
+    }
+}
---
+++ Services/Catalog/ShopApp.Catalog/Dtos/ProductDetailsDtos/ResultProductDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopApp.Catalog.Dtos.ProductDetailsDtos
+{
+    public class ResultProductDetailDto
+    {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInformation { get; set; }
+        public string ProductId { get; set; }
+    }
+}
