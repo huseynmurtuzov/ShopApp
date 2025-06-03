3dd2569157318176ddaf6b70ab93cfc842a0a166
huseynmurtuzov <murtuzovhuseyn2@gmail.com>
2025-06-03 23:01:20 +0400

    some fixes

--- Services/Catalog/ShopApp.Catalog/obj/Debug/net8.0/ShopApp.Catalog.AssemblyInfo.cs
+++ Services/Catalog/ShopApp.Catalog/obj/Debug/net8.0/ShopApp.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopApp.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b46a342aca8103f021710343d2ded35836a121a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda07f95328119a6c1721c2a6a8c4b5cacb93ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopApp.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopApp.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
